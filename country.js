3b4ab40f863cd4d45d20988d83719c4cfbc430ae
Diana Cirjaliu <Diana.Cirjaliu@cognizant.com>
2026-01-15 16:36:04 +0200

    Initial commit

---
+++ country.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const btn = document.querySelector('#button');
+const searchInput = document.querySelector('#searchInput');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags?.png}" />
+    <div class="country__name-region">
+      <h3 class="country__name">${data.name?.common}</h3>
+      <h3 class="country__capital">Capital: ${data.capital}</h3>
+      <h3 class="country__region">Region: ${data.region}</h3>
+      <p><a href="${data.maps?.googleMaps}">Google Maps</a></p>
+    </div>
+    <div class="country__data">
+      <h3 class="country__row">Population: ${(data.population / 1_000_000).toFixed(1)} mil</h3>
+       <h3 class="country__row">Language: ${Object.values(data.languages || {}).join(', ')}</h3>
+      <h3 class="country__row">Currency: ${Object.values(data.currencies || {})[0]?.name || '—'}</h3>
+    </div>
+  </article>
+  `;
+  countriesContainer.innerHTML = html;
+  countriesContainer.style.opacity = 1;
+};
+
+const displyCountry = function (country) { 
+    fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+btn.addEventListener('click', function() {
+  const country = searchInput.value;
+  if (country) {
+    displyCountry(country);
+  }
+}); 
+
+const searchButton = document.getElementById('searchInput');
+const button = document.getElementById('button');
